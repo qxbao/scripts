73d15a080fa2f29bdb67bf3f621dd31f8de5f566
orca <44576398+qxbao@users.noreply.github.com>
2023-03-04 10:07:17 +0700

    Create hello.user.js

---
+++ hello.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Why Hello?
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Skip all Hello assessments
+// @author       Orca
+// @match        https://hello.hanu.edu.vn/web/mod/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hanu.edu.vn
+// @require      https://code.jquery.com/jquery-3.6.3.min.js
+// @downloadURL  https://raw.githubusercontent.com/qxbao/scripts/main/hello.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("Your WhyHello script is out of date. Please uninstall it.");
+})
+})();
