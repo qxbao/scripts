1ad179f30df3c940df5397498dd42646681d9d11
orca <44576398+qxbao@users.noreply.github.com>
2023-05-17 18:35:15 +0700

    Update whyhello.user.js

--- whyhello.user.js
+++ whyhello.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Why Hello?
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  Skip all Hello assessments
 // @author       Orca
 // @match        https://hello.hanu.edu.vn/web/mod/*
@@ -16,45 +16,59 @@
 
     // Your code here...
     let counter = 0;
-    let time = 2500;
-    
+    // Customisable
+    let gap = 2500;
+
+    const StartAttempt = $('input[value="Start attempt"]');
+    const Continue = $('input[value="Continue"]');
+    const Next = $("#mod_quiz-next-nav");
+    const NextAct = $('#next-activity-link');
+    const AddSubmission = $('input[value="Add submission"]');
+
     const func = () => {
-        if ($('input[value="Start attempt"]').length){
-            $('input[value="Start attempt"]').click();
-        }else if($('input[value="Continue"]').length){
-            $('input[value="Continue"]').click();
+
+        if(StartAttempt.length){
+            StartAttempt.click();
         }
-        else if($("#mod_quiz-next-nav").length) {
-            $("#mod_quiz-next-nav").click();
-        }else if($('input[value="Submit all and finish"]').length){
+        else if(Continue.length){
+            Continue.click();
+        }
+        else if(Next.length) {
+            Next.click();
+        }
+        else if($('input[value="Submit all and finish"]').length){
             $('input[value="Submit all and finish"]').click();
-        }else if($('.controls').eq(1).find('button').length || $('.quizstartbuttondiv form button').length){
+        }
+        else if($('.controls').eq(1).find('button').length || $('.quizstartbuttondiv form button').length){
             $('.quizstartbuttondiv form button').click();
             $('.controls').eq(1).find('button').click();
-        }else if ($('#next-activity-link').length) {
-            $('#next-activity-link').get(0).click();
-        }else if ($('input[value="Add submission"]').length){
+        }
+        else if (NextAct.length) {
+            NextAct.get(0).click();
+        }
+        else if (AddSubmission.length){
             $('input[value="Submit all and finish"]').click();
-        }else {
+        }
+        else {
             document.title = "!!!!!!!!!!!!!!";
         }
     }
     
-    func();
-    
     $(document).ready(() => {
-    let cycle = setInterval(() => {
-        counter++;
-        if (counter > 2) {
-            clearInterval(cycle);
-            return;
-        }
-        func();
-    }, time)
-})
+        let cycle = setInterval(() => {
+            counter++;
+            if (counter > 2) {
+                clearInterval(cycle);
+                return;
+            }
+            func();
+        }, gap)
+    })
+
 // Changelog:
 // 1.1: Fix some minor bugs
 // 1.2: Click limit to 3 to avoid crash and add cycle-controller
 // 1.3: WhyHello turn into a private tool
 // 1.4: Add new C1 event
+// 1.5: Add new event, fix click limitation and optimise code
 })();
