29738a19da0649c4c9639c67f84ba58bd01bc7af
orca <44576398+qxbao@users.noreply.github.com>
2024-07-30 23:42:55 +0700

    Update autonfa_stable.user.js

--- autonfa_stable.user.js
+++ autonfa_stable.user.js
@@ -1,59 +1,64 @@
-// ==UserScript==
-// @name         AUTOnfa Stable
-// @namespace    http://tampermonkey.net/
-// @version      1.6
-// @description  Automation for Onfa.io
-// @author       Orca
-// @match        https://onfa.io/ecosystem/mining
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=onfa.io
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-    const clickMiners = (miners, counter) => {
-        console.log(`AUTOnfa debugger: Click at ${new Date().toLocaleTimeString()} (${counter})`);
-        if (counter % 10 == 0) {
-            console.log("AUTOnfa debugger: Reloading miners");
-            $("#reloadListing")[0].click();
-        }
-        for (const miner of miners) {
-            console.log($("#buttonClaim" + miner).length == 0 ? `>> #${miner} not available` : `>> #${miner} done`);
-            $("#buttonClaim" + miner).click();
-        }
-        return counter + 1;
-    }
-    const dprint = (msg) => {
-        console.log("AUTOnfa Debugger >> " + msg);
-    }
-    const resting = 15000;
-    let counter = 1;
-    $(document).ready(async () => {
-        dprint("Started")
-        dprint("Initializing...");
-        const miners = [];
-        const initPromise = new Promise((res) => {
-            const initerval = setInterval(() => {
-                dprint("Looking for miners data...");
-                const minersText = $(".detail div strong");
-                for (const minerText of minersText) {
-                    miners.push(minerText.textContent.split("#")[1].trim());
+    // ==UserScript==
+    // @name         AUTOnfa Stable
+    // @namespace    http://tampermonkey.net/
+    // @version      1.6
+    // @description  Automation for Onfa.io
+    // @author       Orca
+    // @match        https://onfa.io/ecosystem/mining
+    // @require      https://code.jquery.com/jquery-3.7.1.min.js
+    // @icon         https://www.google.com/s2/favicons?sz=64&domain=onfa.io
+    // @grant        none
+    // ==/UserScript==
+    
+    (function() {
+        'use strict';
+        const clickMiners = (miners, counter) => {
+            console.log(`AUTOnfa debugger: Click at ${new Date().toLocaleTimeString()} (${counter})`);
+            if (counter % 10 == 0) {
+                console.log("AUTOnfa debugger: Reloading miners");
+                $("#reloadListing")[0].click();
+            }
+            for (const miner of miners) {
+                if ($("#buttonClaim" + miner).length == 0 && $("#buttonMine" + miner).length == 0)
+                    console.log(`>> #${miner} not available`);
+                else {
+                    console.log(`>> #${miner} done`);
+                    $("#buttonClaim" + miner).click();
+                    $("#buttonMine" + miner).click();
                 }
-                if (miners.length > 0) {
-                    dprint("Init successfully.");
-                    clearInterval(initerval);
-                    res();
-                } else {
-                    dprint("Init failed.");
-                    $("#reloadListing")[0].click();
-                }
-            }, 1000);
+            }
+            return counter + 1;
+        }
+        const dprint = (msg) => {
+            console.log("AUTOnfa Debugger >> " + msg);
+        }
+        const resting = 15000;
+        let counter = 1;
+        $(document).ready(async () => {
+            dprint("Started")
+            dprint("Initializing...");
+            const miners = [];
+            const initPromise = new Promise((res) => {
+                const initerval = setInterval(() => {
+                    dprint("Looking for miners data...");
+                    const minersText = $(".detail div strong");
+                    for (const minerText of minersText) {
+                        miners.push(minerText.textContent.split("#")[1].trim());
+                    }
+                    if (miners.length > 0) {
+                        dprint("Init successfully.");
+                        clearInterval(initerval);
+                        res();
+                    } else {
+                        dprint("Init failed.");
+                        $("#reloadListing")[0].click();
+                    }
+                }, 1000);
+            })
+            await initPromise;
+            dprint("Miners(" + miners.length +") = " + miners.map(e => "#" + e).join(" "));
+            const cycle = setInterval(() => {
+                counter = clickMiners(miners, counter);
+            }, resting);
         })
-        await initPromise;
-        dprint("Miners(" + miners.length +") = " + miners.map(e => "#" + e).join(" "));
-        const cycle = setInterval(() => {
-            counter = clickMiners(miners, counter);
-        }, resting);
-    })
-})();
+    })();
