d9833baa6cbce9bbf07a13e2b3cdbabec75b9842
orca <44576398+qxbao@users.noreply.github.com>
2025-09-15 20:00:31 +0700

    Update onfacamper.user.js

--- onfacamper.user.js
+++ onfacamper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OnfaCamper
 // @namespace    http://tampermonkey.net/
-// @version      2025-01-17
+// @version      2025-09-15
 // @description  Tự động mua tranh
 // @author       You
 // @match        https://onfa.io/ecosystem/nft
@@ -15,13 +15,19 @@
 (async function() {
     'use strict';
     const delayTime = 2000;
-    const price = 50;
+    const price = 1000;
     let wait = 0;
     const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     const click = async (element) => {
         const event = new Event("click", { bubbles: true })
         element.dispatchEvent(event);
         await sleep(500);
+    }
+    const getEl = () => {
+        const nfts = Array.from(document.getElementById("latestNfts").querySelectorAll(`.col-6.item-filter[data-price="${price}"]`)).filter(nft => {
+            return nft.textContent.includes("OHO")
+        });
+        return nfts.length == 0 ? null : nfts[0]
     }
     while (document.getElementById("latestNfts") == null) {
         if (wait > 200) return location.reload();
@@ -30,15 +36,16 @@
     };
     while (document.getElementById("latestNfts").querySelectorAll(`.col-6.item-filter[data-price="${price}"]`) == null) await sleep(50);
     while (document.getElementById("latestNfts").querySelectorAll(`.col-6.item-filter`).length == 0) await sleep(50);
-    const available = document.getElementById("latestNfts").querySelectorAll(`.col-6.item-filter[data-price="${price}"]`);
+    const available = getEl();
     console.log(available);
-    if (available.length == 0) {
+    if (available == null) {
         setTimeout(() => location.reload(), delayTime);
     } else {
-        priceFilter(price, `${price} USDT`);
+        priceFilter(price, `Filtering...`);
         await sleep(1000);
-        while (available[0].querySelector(".bg-nft") == null) await sleep(50);
-        click(available[0].querySelector(".bg-nft"));
+        const el = getEl()
+        while (available.querySelector(".bg-nft") == null) await sleep(50);
+        click(available.querySelector(".bg-nft"));
         while (document.getElementById("modalDetailsNfts").querySelector("button") == null) await sleep(50);
         click(document.getElementById("modalDetailsNfts").querySelector("button"));
     }
